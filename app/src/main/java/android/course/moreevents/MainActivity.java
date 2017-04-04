b0a9be8008df418f69c355ce4a461e71deae9f9d
DanielJakar <danieljakar@gmail.com>
2017-04-04 17:50:03 +0300

    Working Colour picker

--- app/src/main/java/android/course/moreevents/MainActivity.java
+++ app/src/main/java/android/course/moreevents/MainActivity.java
@@ -16,15 +16,25 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     private SeekBar sbGreen;
     private SeekBar sbBlue;
 
+    private TextView tvRed;
+    private TextView tvGreen;
+    private TextView tvBlue;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         tvResult = (TextView) findViewById(R.id.tvResult);
+        tvRed = (TextView) findViewById(R.id.tvRed);
+        tvGreen = (TextView) findViewById(R.id.tvGreen);
+        tvBlue = (TextView) findViewById(R.id.tvBlue);
+
         sbRed = (SeekBar) findViewById(R.id.sbRed);
         sbGreen = (SeekBar) findViewById(R.id.sbGreen);
         sbBlue = (SeekBar) findViewById(R.id.sbBlue);
+
 
         sbRed.setOnSeekBarChangeListener(this);
         sbGreen.setOnSeekBarChangeListener(this);
@@ -37,11 +47,26 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         int rgb = Color.rgb(sbRed.getProgress(),sbGreen.getProgress(),sbBlue.getProgress());
+        int rgbRed = Color.rgb(sbRed.getProgress(),127,127);
+        int rgbGreen = Color.rgb(255,sbGreen.getProgress(),255);
+        int rgbBlue = Color.rgb(255,255,sbBlue.getProgress());
         int rgbText = Color.rgb(256 - sbRed.getProgress(),256 - sbGreen.getProgress(),256 - sbBlue.getProgress());
         tvResult.setBackgroundColor(rgb);
         tvResult.setTextColor(rgbText);
         //tvResult.setTextSize(sbTextSize.getProgress());
-        String s = Integer.toHexString(rgb).toUpperCase();
+
+        String blue = Integer.toString(sbBlue.getProgress());
+
+        tvBlue.setText(blue);
+//        tvBlue.setBackgroundColor(rgbBlue);
+        String red = Integer.toString(sbRed.getProgress());
+        tvRed.setText(red);
+//        tvRed.setBackgroundColor(rgbRed);
+        String green = Integer.toString(sbGreen.getProgress());
+        tvGreen.setText(green);
+//        tvGreen.setBackgroundColor(rgbGreen);
+        String s = "#" + Integer.toHexString(rgb).toUpperCase();
+        tvResult.setText(s);
     }
 
     @Override
