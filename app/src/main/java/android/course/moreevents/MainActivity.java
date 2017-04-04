c316cd2ea8f3e8acba06e0fd349ba17e299a605b
DanielJakar <danieljakar@gmail.com>
2017-04-04 19:39:39 +0300

    attempt to use both

--- app/src/main/java/android/course/moreevents/MainActivity.java
+++ app/src/main/java/android/course/moreevents/MainActivity.java
@@ -3,13 +3,15 @@ package android.course.moreevents;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, TextWatcher {
     //properties:
     private TextView tvResult;
     private SeekBar sbRed;
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     private TextView tvRed;
     private TextView tvGreen;
     private TextView tvBlue;
+    private boolean byUser = true;
 
 
     @Override
@@ -26,6 +29,24 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initLayout();
+        initEvents();
+
+
+    }
+
+    private void initEvents() {
+        sbRed.setOnSeekBarChangeListener(this);
+        sbGreen.setOnSeekBarChangeListener(this);
+        sbBlue.setOnSeekBarChangeListener(this);
+
+        tvRed.addTextChangedListener(this);
+        tvGreen.addTextChangedListener(this);
+        tvBlue.addTextChangedListener(this);
+
+    }
+
+    private void initLayout() {
         tvResult = (TextView) findViewById(R.id.tvResult);
         tvRed = (TextView) findViewById(R.id.tvRed);
         tvGreen = (TextView) findViewById(R.id.tvGreen);
@@ -34,13 +55,6 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         sbRed = (SeekBar) findViewById(R.id.sbRed);
         sbGreen = (SeekBar) findViewById(R.id.sbGreen);
         sbBlue = (SeekBar) findViewById(R.id.sbBlue);
-
-
-        sbRed.setOnSeekBarChangeListener(this);
-        sbGreen.setOnSeekBarChangeListener(this);
-        sbBlue.setOnSeekBarChangeListener(this);
-
-
     }
 
 
@@ -55,16 +69,19 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         tvResult.setTextColor(rgbText);
         //tvResult.setTextSize(sbTextSize.getProgress());
 
-        String blue = Integer.toString(sbBlue.getProgress());
+        if (byUser) {
+            String blue = Integer.toString(sbBlue.getProgress());
 
-        tvBlue.setText(blue);
+            tvBlue.setText(blue);
 //        tvBlue.setBackgroundColor(rgbBlue);
-        String red = Integer.toString(sbRed.getProgress());
-        tvRed.setText(red);
+            String red = Integer.toString(sbRed.getProgress());
+            tvRed.setText(red);
+//        tvRed.setText(String.valueOf(sbRed.getProgress()));
 //        tvRed.setBackgroundColor(rgbRed);
-        String green = Integer.toString(sbGreen.getProgress());
-        tvGreen.setText(green);
+            String green = Integer.toString(sbGreen.getProgress());
+            tvGreen.setText(green);
 //        tvGreen.setBackgroundColor(rgbGreen);
+        }
         String s = "#" + Integer.toHexString(rgb).toUpperCase();
         tvResult.setText(s);
     }
@@ -76,6 +93,38 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        try {
+            int redValue = Integer.valueOf(tvRed.getText().toString());
+            int greenValue = Integer.valueOf(tvRed.getText().toString());
+            int blueValue = Integer.valueOf(tvRed.getText().toString());
+
+            byUser = false;
+            sbRed.setProgress(redValue);
+            sbGreen.setProgress(greenValue);
+            sbBlue. setProgress(blueValue);
+
+            byUser = true;
+
+
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
 
     }
 }
